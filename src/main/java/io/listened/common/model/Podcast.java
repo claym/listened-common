bf1846c4046203e7c91a4274519b677a41ad3346
Clay Mitchell <clay@pfd.net>
2015-06-27 17:06:48 -0400

    added podcast attributes

--- src/main/java/io/listened/common/model/Podcast.java
+++ src/main/java/io/listened/common/model/Podcast.java
@@ -1,6 +1,7 @@
 package io.listened.common.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by Clay on 6/21/2015.
@@ -18,6 +19,41 @@ public class Podcast {
     @Column(name = "feed_url", unique = true, nullable = false)
     String feedUrl;
 
+    @Column(name="copyright")
+    String copyright;
+
+    @Column(name="description")
+    String description;
+
+    @Column(name="docs")
+    String docs;
+
+    @Column(name="language")
+    String language;
+
+    @Column(name="link")
+    String link;
+
+    @Column(name="publish_date")
+    Date publishDate;
+
+    @Column(name="title")
+    String title;
+
+    /** Begin iTunes Stuff **/
+
+    @Column(name="summary")
+    String summary;
+
+    @Column(name="subtitle")
+    String subtitle;
+
+    @Column(name="block")
+    Boolean block;
+
+    @Column(name="explicit")
+    Boolean explicit;
+
     public Long getId() {
         return id;
     }
@@ -32,5 +68,93 @@ public class Podcast {
 
     public void setFeedUrl(String feedUrl) {
         this.feedUrl = feedUrl;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDocs() {
+        return docs;
+    }
+
+    public void setDocs(String docs) {
+        this.docs = docs;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public Boolean getBlock() {
+        return block;
+    }
+
+    public void setBlock(Boolean block) {
+        this.block = block;
+    }
+
+    public Boolean getExplicit() {
+        return explicit;
+    }
+
+    public void setExplicit(Boolean explicit) {
+        this.explicit = explicit;
     }
 }

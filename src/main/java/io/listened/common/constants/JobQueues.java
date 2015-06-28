23fa8425f5e0f996449d7c2fd7fa499000eaf414
Clay Mitchell <clay@pfd.net>
2015-06-28 14:33:28 -0400

    Added new queues

--- src/main/java/io/listened/common/constants/JobQueues.java
+++ src/main/java/io/listened/common/constants/JobQueues.java
@@ -2,8 +2,15 @@ package io.listened.common.constants;
 
 public class JobQueues {
 
+
+    // podcast feed add / update
     public static final String JOB_PODCAST_ADD = "job.podcast.add";
     public static final String JOB_PODCAST_REFRESH = "job.podcast.refresh";
+
+    public static final String JOB_PODCAST_PROCESS = "job.podcast.process";
+    public static final String JOB_EPISODE_PROCESS = "job.episode.process";
+
+    // iTunes related stuff
     public static final String JOB_ITUNES_GENRE = "job.itunes.genre";
 
 }
